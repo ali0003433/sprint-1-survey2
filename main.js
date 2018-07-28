b54676e9a7e95358e8aa415e6141b1e06cdc2528
AL <alyssaliguori@Alyssas-MacBook-Air.local>
2018-07-28 15:00:01 -0400

    play v1

--- main.js
+++ main.js
@@ -1,44 +1,75 @@
-let yourName = prompt("What's your name?");
-let favFood = prompt("What's your favorite food?");
-let agree = confirm("Is your favorite food " + favFood + "?")
-if (agree === true) {
-    (alert("Cool, " + yourName + ". I'm glad you still like " + favFood + " but feel free to change your mind next time."));
-} else {
-    alert("Sounds like you may have changed your mind.");
-    favFood2 = prompt(yourName + ", what are you thinking at moment is your favorite food?");
-    let agree2 = confirm("Is your favorite food " + favFood2 + "?")
-    if (agree2===true){
-        (alert ("Great. Let's get some " + favFood2 + " after all that thinking."))
-    } else {alert("I give up. Both "+  favFood + " and " + favFood2 + " sound like good choices to me.")}
-}
-let elName = document.getElementsByClassName('name');
-    for(i = 0; i < elName.length; i++)
-    {
-        elName[i].innerHTML = yourName;
-    }
-let elFavFood = document.getElementsByClassName('food1');
-for(i = 0; i < elFavFood.length; i++)
-{
-    elFavFood[i].innerHTML = favFood;
-}
-s
-let elConfirm = document.getElementById('confirm');
-elConfirm.innerHTML = agree;
-let elFood2 = document.getElementById('food2');
-elFood2.innerHTML = favFood2;
+// Function called newFunc that accepts parameters. Prompts x and returns y. 
 
+function newFunc(x) { 
+    return prompt(x);
+};
 
-console.log(yourName);
+//Invoke newFunc and return yourName
+let yourName = newFunc('What\'s your name?'); 
+console.log();
+
+//Invoke newFunc and return teacherName
+let teacherName = newFunc('What\'s your Javascript teacher\'s name?'); 
+console.log(teacherName);
+
+//Invoke newFunc and return favFood 
+let favFood = newFunc('What\'s your favorite food?'); 
 console.log(favFood);
+
+//Confirm favFood
+let agree = confirm("Is your favorite food " + favFood + "?")
 console.log(agree);
-console.log(agree2); 
-console.log(favFood2); 
-console.log(elName);
-console.log(elFood);
-console.log(elConfirm);
-console.log(elFood2); 
-console.log(elConfirm2);
-console.log(elFoods);
-console.log(names); 
 
+//If condition is true, invoke alert function and use yourName and favFood variables. 
+let favFood2;
+let agree2;
+if (agree === true)
+{
+    (alert("Great, " + yourName + ". I'm glad you still like " + favFood));
+} 
+//Else, invoke alert then invoke newFunc passing it a string and favFood2 variables. Then, invoke confirm function and set value to agree2. If agree2 condition is met, then invoke one alert. Else, invoke another alert. 
+else 
+{
+    alert("Sounds like you may have changed your mind.");
+    newFunc('What are you thinking at the moment is your favorite food?', favFood2); 
+    agree2 = confirm("Is your favorite food " + favFood2 + "?"); 
+    if (agree2 === true)
+    {
+        (alert ("Great. Let's get some " + favFood2 + " after all that thinking."))
+    }
+    else 
+    {
+        (alert("I give up. Both "+  favFood + " and " + favFood2 + " sound like good choices to me."))
+    }
+}; 
 
+//Create function newFunc2 which uses HTML method and property 
+function newFunc2(l, m) 
+{
+let elOne = document.getElementsByClassName(l)
+    for(i = 0; i < elOne.length; i++)
+    {
+        elOne[i].innerHTML = m; 
+    }; 
+};
+
+//Invoke newFunc2 and pass it arguments 
+newFunc2('name', yourName); 
+console.log(newFunc2);
+newFunc2('food1', favFood); 
+console.log(newFunc2); 
+
+// Function called newFunc2. Gets element by id, using argument id1, and replaces it with a string. Sets element to equal parameter para1. 
+function newFunc3(id1, para1)
+{
+    let elTwo= document.getElementById(id1)
+    elTwo.innerHTML = para1; 
+}; 
+
+// Invoke newFunc2 three times by passing it arguments 
+newFunc3('confirm', agree2);
+console.log(newFunc3);
+newFunc3('food2', favFood2); 
+console.log(newFunc3); 
+newFunc3('teacher', teacherName); 
+console.log(newFunc3); 
